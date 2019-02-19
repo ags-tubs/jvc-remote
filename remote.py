72aef9affdfd30c98db116dbaec0eb4f71176086
Eggert Jung <eggert.s.jung@wischhof13.de>
2019-02-19 21:01:59 +0100

    few new features and shrink down init sequence

--- remote.py
+++ remote.py
@@ -6,6 +6,8 @@ import time
 import numpy as np
 np.set_printoptions(formatter={'int':hex})
 
+verbose = True
+
 key_id = []
 cmd_id = []
 
@@ -20,74 +22,115 @@ with open('cmds.csv') as csvDataFile:
 ser = serial.Serial('/dev/ttyUSB0', 9600, parity=serial.PARITY_EVEN)
 ser.timeout=0.1
 
-await_cam = False
+def connect():
+    connected=False
+    ser.flushInput()
+    while(not connected):
+        ser.write(b'\x80')
+        r=ser.read()
+        if(r == b'\x80'):
+            ser.write(b'\xA0')
+            connected=True
+            print("connected")
+        else:
+            print("waiting")
+        time.sleep(0.8)
+    sendCmd(0,0)
 
 def waitCam(verbose=False):
-    ack=False
-    while(not ack):
+    rcv=False
+    retry_count=0
+    while(not rcv and retry_count < 2):
+        r = ser.read()
         try:
-            r = ord(ser.read())
-#            print("cam: "+hex(r))
-            ack=True
+            r=ord(r)
+            #if verbose:
+            #    print("cam: "+hex(r))
+            rcv=True
         except TypeError:
-            print("waiting for CAM...")
-            ack=False
+            if(retry_count < 1):
+                print("waiting for CAM...")
+            rcv=False
+            retry_count+=1
+            continue
 
+    if(not rcv):
+        if(verbose):
+            print("CAM did not respond ...")
+        return False
     if(r == 0xA0):
         if(verbose):
-            print("\tACK")
+            print("cam: ACK")
+        return True
+    elif((r & 0xF0) == 0xF0):
+        print("application ERROR: "+hex(r))
+        return False
+    handleCam(r)
+
+def handleCam(r):
+    try:
+        r=ord(r)
+        if verbose:
+            print("hadeling cam: "+hex(r))
+    except TypeError:
+        if type(r) is int:
+            pass
+        else:
+            if verbose:
+                print("nothing to handle; going on ...")
+            return
+
+    if(r == 0x83):
+        print("CAM transmitting")
+        packet = []
+        ser.write(b'\xA0')
+        buff = ser.read(3)
+        ser.write(b'\xA0')
+        for x in buff:
+            packet.append(x)
+        if verbose:
+            print(np.array(packet))
+
+        if((packet[0] + packet[1]) & 0x7F != (packet[2])):
+            print("checksum ERROR")
+            print(np.array(packet))
+            ser.flushInput()
+        else:
+            cmd = packet[1]
+            data = (packet[0] & 0x0F)
+            bitflags = (packet[0] & 0xF0) >> 4
+
+            decode(cmd, data, cmd_id)
+
+            if(bitflags & 0x2):
+                print("request special transmission")
+    elif(r==0x80):
+        print("CAM seems to be unconnected!")
+        print("trying to reconnect")
+        connect()
+        SpecialTransmitt(0)
     else:
-        print("\tNACK: "+hex(r))
+        print("Cam sent: "+hex(r))
+        print("unhandled special request response? "+hex(r)+"; sending ACK ...")
+        ser.write(b'\xA0')
+        handleCam(ser.read())
+
 
 def SpecialTransmitt(data):
-    ser.flushInput()
     print("#### Special Transmit beginn ####")
-    tx=0x90|data
-    ser.write(b'\x90')
-    print("write: 0x90")
-    waitCam()
-    waitCam()
-    ser.write(b'\xA0')
-    print("write: 0xA0")
-    waitCam()
-    waitCam()
-    waitCam()
-    ser.write(b'\xA0')
-    print("write: 0xA0")
 
-    waitCam()
-    ser.write(b'\xA0')
-    print("write: 0xA0")
-    waitCam()
-    waitCam()
-    waitCam()
-    ser.write(b'\xA0')
-    print("write: 0xA0")
+    handleCam(ser.read())
+    tx=0x90|data
+    print("write: "+hex(tx))
+    ser.write(bytearray([tx]))
+    waitCam(True)
+    waitCam(True)
 
     print("#### Special Transmit end ####")
 
-def autowhite():
-    tmp=ser.timeout
-    ser.timeout=3
-    sendCmd("Auto White", 0x1)
-    ans=ord(ser.read())
-    ser.write(b'\xA0')
-    ser.timeout=tmp
-
-    if(ans==0x90):
-        print("Auto White OK")
-    elif(ans==0x92):
-        print("Auto White failed")
-    elif(ans==0x94):
-        print("Auto White failed: Over Light")
-    elif(ans==0x96):
-        print("Auto White failed: Improper Object")
-    else:
-        print("Unknown Answer...")
-        print(hex(ans))
-
 def sendCmd(cmd, data, special=False):
-    print("sendCmd "+str(cmd)+" = "+str(data)+"\t",end='')
+    handleCam(ser.read())
+    print("sendCmd "+str(cmd)+" = "+str(data), end='')
 
     if(type(cmd) is str):
         found = False
@@ -109,22 +152,22 @@ def sendCmd(cmd, data, special=False):
             print("unknown string: \""+data+"\"")
             return
 
-    print(np.array([cmd, data]),end='')
-
     tx=[data|0x40,cmd,0]
     if(special):
         tx[0]=tx[0]|0x2
     tx[2]=(tx[0]+tx[1])&0x7F
 
     ser.write(b'\x83')
-    #time.sleep(0.003)
-    waitCam()
+    if(not waitCam()):
+        sendCmd(cmd, data, special)
     ser.write(tx)
-    #time.sleep(0.01)
-    waitCam(True)
+    print(np.array(tx),end='\t')
+    if(waitCam(True)):
+        print(hex(data));
 
 def sendKey(key, val):
-    print("sendKey "+str(key)+" = "+str(val),end='')
+    handleCam(ser.read())
+    print("sendKey "+str(key)+" = "+str(val), end='\t')
     if(type(key) is str):
         found = False
         for x in key_id:
@@ -151,252 +194,95 @@ def sendKey(key, val):
     tx[3]=(tx[0]+tx[1]+tx[2])&0x7F
 
     ser.write(b'\x84')
-    #time.sleep(0.003)
-    waitCam()
+    if(not waitCam()):
+        print("aborting")
+        return
     ser.write(tx)
-    #time.sleep(0.01)
-    waitCam(True)
+    print(np.array(tx),end='\t')
+    if(not waitCam(True)):
+        print("aborting")
+        return
+        #sendKey(key, val)
 
 def init_cam():
-    connected=False
-    while(not connected):
-        ser.write(b'\x80')
-        r=ser.read()
-        if(r == b'\x80'):
-            ser.write(b'\xA0')
-            connected=True
-            print("connected")
-        else:
-            print("waiting")
-        time.sleep(0.8)
-
-    sendCmd(0x7d, 0x1)
-
-    sendCmd("Colorbars", 0x0)
-
-    sendCmd(0x51, 0x1)	#ACK UNIPLM
-
-    sendCmd("White Bal", "A")
-
-    sendCmd("Autoiris", 0x0)
-
-    sendKey("Iris", 0x340)
-
-    sendCmd("Autoiris", 0x1)
-
-    sendKey("Autoiris", 0x340)
-
-    sendCmd("Autoiris", 0x1)
-
-    sendCmd("Auto White", 0x1,True)#request special transmission
-
-
-    sendCmd("White Bal", "Preset")
-
-    sendCmd("Shutter", "off")
-
-    sendCmd("Shutter", 0x1)	#ACK UNIPLM
-
-    sendCmd("Shutter", "1/120")
-
-    sendCmd("Shutter", "1/250")
-
-    sendCmd("Shutter", "1/500")
-
-    sendCmd("Shutter", "1/1000")
-
-    sendCmd("Shutter", "1/2000")
-
-    sendCmd("Shutter", 0xf)	#ACK UNIPLM
-
-    sendKey(0x56, 0x26e)	#ACK UNIPLM
-
-    sendCmd("Shutter", "off")
-
-    sendKey("Black", 0x310)
-
-    sendCmd("Auto White", 0x0,True)#request special transmission
-    	#ACK UNIPLM
-
-    sendCmd("White Bal", 0x0)
-
-    sendCmd("White Bal", "Preset")
-
-    sendCmd("White Bal", "A")
-
-    sendCmd("White Bal", "B")
-
-    sendCmd("White Bal", "full auto")
-
-    sendCmd("White Bal", "A")
-
-    sendKey("Red", 0x328)
-
-    sendKey("Blue", 0x230)
-
-    sendCmd("White Bal", "B")
-
-    sendKey("Red", 0x404)
-
-    sendKey("Blue", 0x41c)
-
-    sendCmd("White Bal", 0x0)
-
-    sendKey(0x02, 0x33c)
-
-    sendKey(0x03, 0x368)
-
-    sendCmd("White Bal", "Preset")
-
-    sendCmd("Gain", "0dB")
-
-    sendCmd("Gain", "6dB")
-
-    sendCmd("Gain", "9dB")
-
-    sendCmd("Gain", "12dB")
-
-    sendCmd("Gain", "18dB")
-
-    sendCmd("Gain", 0x5)	#ACK UNIPLM
-
-    sendCmd("Gain", 0x6)	#ACK UNIPLM
-
-    sendCmd("Gain", 0x8)
-
-    sendCmd("Gain", "3dB")
-
-    sendCmd("Gain", 0xe)	#ACK UNIPLM
-
-    sendKey(0x74, 0x801)	#ACK UNIPLM
-
-    sendCmd(0x51, 0x1)	#ACK UNIPLM
-
-    sendCmd(0x51, 0x0)	#ACK UNIPLM
-
-    sendCmd("Gain", "0dB")
-
-    sendCmd(0x08, 0x1)
-
-    sendCmd(0x53, 0x1)
-
-    sendCmd(0x08, 0x0)
-
-    sendCmd(0x53, 0x0)
-
-    sendCmd("Call", 0x1)
-
-    sendCmd("Call", 0x0)
-
-    sendCmd("black mod", "stretch")
-
-    sendCmd("black mod", "compress")
-
-    sendCmd("black mod", "unmod")
-
-    sendCmd(0x4b, 0x0)	#ACK UNIPLM
-
-    sendCmd(0x4b, 0x2)	#ACK UNIPLM
-
-    sendCmd(0x4b, 0xf)	#ACK UNIPLM
-
-    sendCmd(0x4b, 0x1)	#ACK UNIPLM
-
-    sendCmd(0x4c, 0x0)
-
-    sendCmd(0x4c, 0x2)
-
-    sendCmd(0x4c, 0xf)	#ACK UNIPLM
-
-    sendCmd(0x4c, 0x1)
-
-    sendCmd(0x4f, 0x1)	#ACK UNIPLM
-
-    sendCmd(0x4f, 0x2)	#ACK UNIPLM
-
-    sendCmd(0x4f, 0x0)	#ACK UNIPLM
-
-    sendCmd(0x1b, 0x1)	#ACK UNIPLM
-
-    sendCmd(0x1b, 0x0)	#ACK UNIPLM
-
-    sendKey(0x12, 0x530)	#ACK UNIPLM
-
-    sendCmd("Detail", 0x0)
-
-    sendKey(0x14, 0x400)
-
-    sendKey(0x16, 0x400)
-
-    sendCmd(0x4c, 0x1)
-
-    sendCmd("Skin Detail", 0x0)
-
-    sendCmd("Auto Knee", 0x0)
-
-    sendKey(0x32, 0x604)
-
-    sendCmd(0x4e, 0x1)	#ACK UNIPLM
-
-    sendCmd(0x1b, 0x0)	#ACK UNIPLM
-
-    sendCmd(0x1d, 0x0)
-
-    sendKey(0x36, 0x114)
-
-    sendCmd("black mod", "unmod")
-
-    sendCmd(0x4f, 0x0)	#ACK UNIPLM
-
-    sendCmd("DNR", 0x0)
-
-    sendCmd(0x4b, 0x1)	#ACK UNIPLM
-
-    sendCmd(0x52, 0x0)
-
-    sendKey(0xf3, 0xf7c)	#ACK UNIPLM
-
-    sendKey(0xf2, 0x8)	#ACK UNIPLM
-
-    sendKey(0xf2, 0xc)	#ACK UNIPLM
-
-    sendKey(0xf2, 0x80c)	#ACK UNIPLM
-
-    sendKey(0xf3, 0xc)	#ACK UNIPLM
-
-    sendCmd("Gain", "0dB")
-
-    sendCmd("Colorbars", 0x0)
-
-    sendCmd(0x50, 0x1)
-
+    ser.flushInput()
+    connect()
     SpecialTransmitt(0)
 
-    sendCmd("Call", 0x0)
+def autowhite():
+    tmp=ser.timeout
+    ser.timeout=3
+    sendCmd("Auto White", 0x1)
+    ans=ord(ser.read())
+    ser.write(b'\xA0')
+    ser.timeout=tmp
 
-    sendCmd("Gain", "0dB")
-
-    sendCmd(0x53, 0x0)
-
-    sendCmd(0x08, 0x0)
+    if(ans==0x90):
+        print("Auto White OK")
+    elif(ans==0x92):
+        print("Auto White failed, maybe Low Light or in Preset mode")
+    elif(ans==0x94):
+        print("Auto White failed: Over Light")
+    elif(ans==0x96):
+        print("Auto White failed: Improper Object")
+    else:
+        print("Unknown Answer...")
+        print(hex(ans))
 
     sendCmd("Full Auto Shooting", 0x0)
 
-#while True:
-#    time.sleep(1)
-#    sendCmd(0x0,0x1)
-#    time.sleep(1)
-#    sendCmd(0x0,0x0)
-#    time.sleep(1)
-#    sendCmd("Gain", "0dB")
-#    time.sleep(1)
-#    sendCmd("Gain", "3dB")
-#    time.sleep(1)
-#    sendCmd("Gain", "6dB")
-#    time.sleep(1)
-#    sendCmd("Gain", "9dB")
-#    time.sleep(1)
-#    sendCmd("Gain", "12dB")
-#    time.sleep(1)
-#    sendCmd("Gain", "18dB")
+def fun():
+    while True:
+        time.sleep(1)
+        sendCmd(0x0,0x1)
+        time.sleep(1)
+        sendCmd(0x0,0x0)
+        time.sleep(1)
+        sendCmd("Gain", "0dB")
+        time.sleep(1)
+        sendCmd("Gain", "3dB")
+        time.sleep(1)
+        sendCmd("Gain", "6dB")
+        time.sleep(1)
+        sendCmd("Gain", "9dB")
+        time.sleep(1)
+        sendCmd("Gain", "12dB")
+        time.sleep(1)
+        sendCmd("Gain", "18dB")
+
+def decode(feature_id, value, table):
+    found = False
+    for x in table:
+        if((int(x[1],0)==feature_id) and (int(x[2],0)==value) and (x[6] != "") and not found):
+            found=True
+            if(x[5]==""):
+                #topic="ID \'"+x[1]+"\'"
+                topic=x[1]
+            else:
+                topic="\""+x[5]+"\""
+
+            #print("send"+x[0]+"("+topic+", \""+x[6]+"\")\t")
+            print("set "+topic+" to "+x[6])
+            break
+    if(not found):
+        for x in table:
+            if(int(x[1],0)==feature_id and not found):
+                found=True
+                if(x[5]==""):
+                    #topic="ID \'"+x[1]+"\'"
+                    topic=x[1]
+                else:
+                    topic="\""+x[5]+"\""
+
+                #print("send"+x[0]+"("+topic+", "+hex(value)+")", end='')
+                print("set "+topic+" to \'"+hex(value)+"\'")
+                break
+    if(not found):
+        print("set ID '"+hex(feature_id)+"' to \'"+hex(value)+"\'")
+        #print("send"+x[0]+"("+hex(feature_id)+", "+hex(value)+")", end='')
+    #print("send"+x[0]+"("+hex(feature_id)+","+hex(value)+")")
+
+def getGainValues():
+    for x in range(0, 0xf):
+        sendCmd("Gain",x)
 
