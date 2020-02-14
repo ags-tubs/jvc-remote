6147331149e19bafbd8a1462d07802ecf4265e84
Eggert Jung <eggert.s.jung@wischhof13.de>
2020-02-14 17:16:13 +0100

    transmitting 'a' on 9600 baud

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+#define TX_PIN 2
+
+uint8_t tx_bit_state = 0;
+uint8_t tx_byte = 'a';
+
+void tx(){
+    if(tx_bit_state == 0)
+        PORTB &= ~(1 << TX_PIN);
+    else if(tx_bit_state < 9)
+        PORTB = ((tx_byte >> tx_bit_state-1) & 0x01)<<TX_PIN;
+    else
+        PORTB |= (1 << TX_PIN);
+}
+
 int main(void){
     CCP = 0xD8;
     CLKPSR = 0;
@@ -8,10 +22,10 @@ int main(void){
 
     OSCCAL = 0x82;
     //TCCR0A |= (1 << WGM00);
-    TCCR0B |= (1 << WGM02) | (1 << CS00);
+    TCCR0B |= (1 << WGM02) | (1 << CS01);
     TIMSK0 |= (1 << OCIE0A) | (1 << TOIE0);
 
-    OCR0A = 100;
+    OCR0A = 104;
 
     sei();
     for(;;){
@@ -20,6 +34,7 @@ int main(void){
 }
 
 ISR(TIM0_COMPA_vect){
-    PORTB ^= (1 << 2);
+    tx_bit_state++;
+    tx();
 }
 
